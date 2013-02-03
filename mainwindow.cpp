1bb96e4cc6ada58096c949b7732e35f6808aa256
Tomasz Ducin <tomasz.ducin@gmail.com>
2013-02-03 22:33:52 +0100

    about message window displayed

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QtGui>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +13,11 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::displayAbout()
+{
+  QMessageBox::about(this, tr("About this demo"),
+    tr("<p>This is a nice demo with <i>some</i> "
+       "<u>crazy</u> HTML content.</p>"));
 }
