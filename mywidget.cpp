d9fae86f91d00848be319f036a56bcbad923c845
Simon Kronstein <skronstein7@gmail.com>
2017-12-06 12:37:40 -0800

    display :01m instead of just :0s when the # if mins is a single digit #

--- mywidget.cpp
+++ mywidget.cpp
@@ -89,13 +89,15 @@ void MyWidget::updateTimeDisplay(){
 
     //mins
     textToDisplay.append(QString::number(m) + "m");
+    if(m < 10 && h != 0) textToDisplay.insert(textToDisplay.length()-2, "0"); //to display :01m instead of just :0s when the # if mins is a single digit #
 
     //seconds
     if(sDisplay > 0){
         if(sDisplay == 1 ) timeTo /= 10; //to remove the last digit of the number of seconds
         textToDisplay.append(" : " + QString::number(timeTo)+ "s");
         if(timeTo < 10) textToDisplay.insert(textToDisplay.length()-2, "0"); //to display :01s instead of just :0s when the # if seconds is a single digit #
-    }
+       }
+    qDebug() << "h is " << h;
 
     ui->timeDisplay->setText(textToDisplay);
 
